63401a8df30f19ea600a0123fedc6b144ad818dd
jainkeval <kevalkale17@gmail.com>
2018-03-24 14:12:53 +0530

    Completed API

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    bodyParser = require('body-parser'),
+    todoRoutes = require('./routes/todos');
 
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
 	res.send("landing");
 })
+
+app.use('/todos', todoRoutes);
 
 
 
---
+++ models/index.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+mongoose.set('debug', true);
+mongoose.connect('mongodb://localhost/api-todo');
+
+mongoose.Promise = Promise;
+
+module.exports.Todo = require('./todo')
